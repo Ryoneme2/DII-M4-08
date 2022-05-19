71cc4f50321b610a38225a2648fabb8a78ea6a5e
Ryoneme2 <worknarair6@gmail.com>
2022-05-19 11:18:25 +0700

    config eslint

--- js.js
+++ js.js
@@ -1,12 +1,7 @@
-const ok = document.getElementById('ok');
-const cancel = document.getElementById('cancel');
-const no = document.getElementById('no');
-const all = document.querySelectorAll('.flex-item')
+// const ok = document.getElementById('ok');
+// const cancel = document.getElementById('cancel');
+// const no = document.getElementById('no');
+// const all = document.querySelectorAll('.flex-item')
 
-const arrAll = Array.from(all);
-arrAll.forEach((v) => {
-  v.addEventListener('click', (e) => {
-    alert(v.innerHTML)
-    console.log("%c" + v.innerHTML, "font: 80px sans-serif; font-weight:bold; color:red; -webkit-text-stroke:1px black;");
-  })
-})
+
+// console.log(myName);
