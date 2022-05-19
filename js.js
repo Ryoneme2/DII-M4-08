45a455a11465da688738d0beb84d2ef87129e956
Ryoneme2 <worknarair6@gmail.com>
2022-05-19 12:13:39 +0700

    change logic to chane margin

--- js.js
+++ js.js
@@ -3,14 +3,14 @@ const cancel = document.getElementById('cancel');
 const no = document.getElementById('no');
 // // const all = document.querySelectorAll('.flex-item')
 
+cancel.style.marginLeft = '20px'
+
 ok.addEventListener('click', ()=>{
   // make margin left and margin right gradually increase
-  let defM = 20
-  while(defM < 200) {
-    cancel.style.marginLeft = `${defM}px`;
-    cancel.style.marginRight = `${defM}px`;
-    defM += 1;
-  }
+  let defM = parseInt(cancel.style.marginLeft.split('px'));
+  defM += 20;
+  cancel.style.marginLeft = `${defM}px`;
+  cancel.style.marginRight = `${defM}px`;
 })
 
 const arrWord = ['ok', 'วิชานี้','ง่าย', 'จริงๆนะ']
