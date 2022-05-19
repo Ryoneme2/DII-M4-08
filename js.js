f66d1234a9bb6aba557819de4f5957057aa94d38
Ryoneme2 <worknarair6@gmail.com>
2022-05-19 10:48:17 +0700

    click for alert inner text

--- js.js
+++ js.js
@@ -1,11 +1,12 @@
-const ok = document.getElementById("ok");
-const cancel = document.getElementById("cancel");
+const ok = document.getElementById('ok');
+const cancel = document.getElementById('cancel');
+const no = document.getElementById('no');
+const all = document.querySelectorAll('.flex-item')
 
-ok.addEventListener('mouseover', (e) => {
-  console.log(e);
-})
-
-cancel.addEventListener('mousemove', (e) => {
-  console.log("X : " + e.clientX);
-  console.log("Y : " + e.clientY);
+const arrAll = Array.from(all);
+arrAll.forEach((v) => {
+  v.addEventListener('click', (e) => {
+    alert(v.innerHTML)
+    console.log("%c" + v.innerHTML, "font: 80px sans-serif; font-weight:bold; color:red; -webkit-text-stroke:1px black;");
+  })
 })
