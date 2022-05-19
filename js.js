2dd88a6500c72e0f49a1164392c8f4c4ba3f397f
Ryoneme2 <worknarair6@gmail.com>
2022-05-19 11:30:37 +0700

    when click ok margin will grandually increase

--- js.js
+++ js.js
@@ -1,7 +1,14 @@
-// const ok = document.getElementById('ok');
-// const cancel = document.getElementById('cancel');
+const ok = document.getElementById('ok');
+const cancel = document.getElementById('cancel');
 // const no = document.getElementById('no');
 // const all = document.querySelectorAll('.flex-item')
 
-
-// console.log(myName);
+ok.addEventListener('click', ()=>{
+  // make margin left and margin right gradually increase
+  let defM = 20
+  while(defM < 200) {
+    cancel.style.marginLeft = `${defM}px`;
+    cancel.style.marginRight = `${defM}px`;
+    defM += 1;
+  }
+})
