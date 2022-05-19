85eb29e795a313cf2eb0bbed5a520b58bbe02c8e
Ryoneme2 <worknarair6@gmail.com>
2022-05-19 15:24:00 +0700

    onBlur

--- js.js
+++ js.js
@@ -1,50 +1,22 @@
 /* eslint-disable no-unused-vars */
-const ok = document.getElementById('ok');
-const cancel = document.getElementById('cancel');
-const no = document.getElementById('no');
-const box = document.getElementById('box');
-const all = document.querySelectorAll('.flex-item')
-const cont = document.getElementById('container');
+const searchButton = document.getElementById('searchButton');
+const inputText = document.getElementById('floatingInput');
+const output = document.getElementById('output');
 
-const cont2 = document.getElementById('container2');
-let counter = 0
+function addText() {
+  let text = inputText.value;
+  let newButton = document.createElement('button');
+  newButton.classList.add('btn')
+  newButton.classList.add('btn-outline-primary')
+  newButton.classList.add('my-2')
+  newButton.setAttribute('type', 'button')
+  newButton.innerText = text
+  output.appendChild(newButton)
 
-function addNode() {
-  const newNode = document.createElement('div');
-  newNode.classList.add('flex-item');
-  newNode.setAttribute('id', counter)
-  counter++
-  newNode.innerText = counter
-  cont2.appendChild(newNode);
-}
-ok.addEventListener('click', (e) => {
-  e.stopPropagation()
-  addNode()
-})
-
-// const addContext = (context) => {
-//   const prevContext = box.innerText;
-//   box.innerText = `${prevContext} ${context}`;
-// }
-
-// for(let el of all) {
-//   el.addEventListener('click', (e) => {
-//     e.stopPropagation()
-//     const text = el.innerText.split('').map((v,i) => {
-//       if(i === 0) {
-//         return v.toUpperCase()
-//       }
-//       return v
-//     }).join('')
-//     addContext(text);
-//   })
-// }
-
-function onDelClick(e) {
-  // e.stopPropagation()
-  // box.innerText = ``;
-  cont2.innerHTML = ''
-  counter = 0
+  inputText.value = '';
 }
 
-document.getElementById('container').onclick = onDelClick
+inputText.addEventListener('blur', (e) => {
+  e.preventDefault()
+  addText()
+});
